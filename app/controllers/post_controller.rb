2da1f134ae4f5bb8260190a3a944b5afe8e467fb
bruno_tripoloni <bruno.tripoloni@gmail.com>
2014-05-24 20:16:17 -0300

    corrections of index

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,6 +1,28 @@
 class PostController < ApplicationController
 	def index
-		@post = Post.all
-		render json: @post
+		d = get_posts
+		render json: d
+	end
+	def create
+		@post = Post.create(params[:post])
+		@post.save
+	end
+	private
+	def get_posts
+		posts = Post.all
+		data = []
+		posts.each do |post|
+			user = User.find_by_id(post.user_id-1)
+			d = [
+				id: post.id,
+				content: post.content,
+				created_at: post.created_at,
+				updated_at: post.updated_at,
+				author_name: user.name,
+				author_email: user.email
+			]
+			data << d
+		end
+		return data
 	end
 end

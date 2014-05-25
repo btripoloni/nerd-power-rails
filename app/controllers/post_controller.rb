4c660f551858faa67834b9a1abaf2a8b48fb6fc9
bruno_tripoloni <bruno.tripoloni@gmail.com>
2014-05-25 11:35:16 -0300

    update PostController

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,29 +1,10 @@
 class PostController < ApplicationController
-	
 	def index
-		d = get_posts
-		render json: d
+		@post = Post.all
+		render json: @post, include: [user:{except: [:created_at, :updated_at]}], except: [:user_id]
 	end
 	def create
 		@post = Post.create(params[:post])
 		@post.save
-	end
-	private
-	def get_posts
-		posts = Post.all
-		data = []
-		posts.each do |post|
-			user = User.find_by_id(post.user_id-1)
-			d = [
-				id: post.id,
-				content: post.content,
-				created_at: post.created_at,
-				updated_at: post.updated_at,
-				author_name: user.name,
-				author_email: user.email
-			]
-			data << d
-		end
-		return data
 	end
 end

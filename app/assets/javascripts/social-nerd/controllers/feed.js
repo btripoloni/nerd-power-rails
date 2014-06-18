0a1dbe75f3535b30eedf1ff77563d1c55f93ff5e
bruno_tripoloni <bruno.tripoloni@gmail.com>
2014-06-18 17:08:29 -0300

    send posts via api

--- app/assets/javascripts/social-nerd/controllers/feed.js
+++ app/assets/javascripts/social-nerd/controllers/feed.js
@@ -1,11 +1,22 @@
-controllers.feedCtrl = ['$scope', function($scope) {
+controllers.feedCtrl = ['$scope','$http', function($scope, $http) {
 
-  $scope.posts = [ { content: "Estou comendo pavê de copo" } ]
+  $http.get('api/post').
+    success(function(data) {
+      $scope.posts = data;
+  });
 
   $scope.createPost = function() {
-    var newPost = { content: $scope.content }
-    $scope.posts.unshift(newPost)
-    $scope.content = null
-  }
+    var post = {"post":{"content": $scope.content}};
+    $http.post("api/post", post).success(function(data){
+      $scope.posts.unshift(data);
+      $scope.content = null;
+      console.log("hello");
+    });
+  };
 
+  $scope.removePost = function(id, index){
+    $http.delete("api/post/"+id).success(function(){
+        $scope.posts.splice(index, 1);
+    });
+  };
 }]

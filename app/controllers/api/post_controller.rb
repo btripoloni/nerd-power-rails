9bb394a00fc1c40ddf7b81944645a3364e155ea2
bruno_tripoloni <bruno.tripoloni@gmail.com>
2014-06-21 18:37:48 -0300

    get user posts via api

--- app/controllers/api/post_controller.rb
+++ app/controllers/api/post_controller.rb
@@ -15,10 +15,17 @@ module Api
         render json: @post, include: [user:{except: [:created_at, :updated_at]}]
       end
     end
+
     def destroy
       @post = current_user.posts.find_by_id(params[:id]).destroy
       head :ok
     end
+
+    def user
+      post = Post.includes(:user).find_by_user_id(params[:id])
+      render json: post, include: [user:{except: [:created_at, :updated_at]}], except: :user_id
+    end
+
     private
     def post_params
       params.require(:post).permit(:content, :user_id)

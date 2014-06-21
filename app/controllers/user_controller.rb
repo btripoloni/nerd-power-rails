ff850cc84d4360f0a8099ef39075ba013d8a6e67
bruno_tripoloni <bruno.tripoloni@gmail.com>
2014-06-20 22:49:50 -0300

    define cookie "id_user"

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
 class UserController < ApplicationController
+  def index
+    cookies[:id_user] = current_user.id
+  end
+
   def profile
     @user = User.find_by!(nickname: params[:nickname])
   end
